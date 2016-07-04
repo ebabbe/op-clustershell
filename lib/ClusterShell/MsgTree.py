cea-hpc/clustershell

MsgTree.py: MsgTreeElem performance improvement
MsgTreeElem.__iter__():

    Call reversed() to get a reverse iterator which is slighly faster
    than previous implementation (casting a MsgTreeElem of 1M lines to
    string is about 15-20% faster).

MsgTreeElem.append():

    Major performance improvement when lines added to the tree from
    different keys (nodes) are the same. Remove dict.setdefault which
    was mistakenly used here, leading the creation of a bunch of useless
    objects when lines were the same (a pretty common pattern!).

    Quick benchmark using code from MsgTreeTest.test_006_scalability():

    (without patch vs. with patch)

    1 node: no difference
    2 nodes, each same 1M lines: 50% faster
    10 nodes, each same 1M lines: 86% faster
    16 nodes, each same 1M lines: 103% faster (2x faster!)

    1000 nodes, each same single line: 68% faster
    1000 nodes, each same 10 lines: 70% faster
    1000 nodes, each same 100 lines: 74% faster
    1000 nodes, each same 500 lines: 60% faster
    1000 nodes, each same 1000 lines: 72% faster
    1000 nodes, each same 10000 lines: 71% faster
    1000 nodes, each same 50000 lines: 79% faster

    10000 nodes, each same single line: 59% faster
    10000 nodes, each same 100 lines: 74% faster
    10000 nodes, each same 1000 lines: 73% faster

Change-Id: I327cc06b48aaae694ed2e5b505484f2c78d382d0

lib/ClusterShell/MsgTree.py
@@ -1,6 +1,6 @@
 #
-# Copyright CEA/DAM/DIF (2007-2014)
-#  Contributor: Stephane THIELL <stephane.thiell@cea.fr>
+# Copyright CEA/DAM/DIF (2007-2016)
+#  Contributor: Stephane THIELL <sthiell@stanford.edu>
 #
 # This file is part of the ClusterShell library.
 #
@@ -70,7 +70,7 @@ class MsgTreeElem(object):
         # content
         self.msgline = msgline
         self.keys = None
-   
+
     def __len__(self):
         """Length of whole message string."""
         return len(str(self))
@@ -92,7 +92,7 @@ class MsgTreeElem(object):
             shifting = self.keys
             self.keys = None
         else:
-            shifting = set([ key ])
+            shifting = set([key])
             if self.keys:
                 self.keys.difference_update(shifting)
 
@@ -107,7 +107,7 @@ class MsgTreeElem(object):
         """Shift one of our key to specified target element (trace
         mode: keep backtrace of keys)."""
         if not target_elem.keys:
-            target_elem.keys = set([ key ])
+            target_elem.keys = set([key])
         else:
             target_elem.keys.add(key)
         return target_elem
@@ -116,24 +116,18 @@ class MsgTreeElem(object):
         return list(self.lines())[i]
 
     def __iter__(self):
-        """Iterate over message lines starting from this tree element."""
-        # no msgline in root element
-        if self.msgline is None:
-            return
-        # trace the message path
-        path = [self.msgline]
-        parent = self.parent
-        while parent.msgline is not None:
-            path.append(parent.msgline)
-            parent = parent.parent
-        # rewind path
-        while path:
-            yield path.pop()
+        """Iterate over message lines up to this element."""
+        bottomtop = []
+        if self.msgline is not None:
+            bottomtop.append(self.msgline)
+            parent = self.parent
+            while parent.msgline is not None:
+                bottomtop.append(parent.msgline)
+                parent = parent.parent
+        return reversed(bottomtop)
 
     def lines(self):
-        """
-        Get the whole message lines iterator from this tree element.
-        """
+        """Get an iterator over all message lines up to this element."""
         return iter(self)
 
     splitlines = lines
@@ -154,14 +148,23 @@ class MsgTreeElem(object):
         Return corresponding MsgTreeElem (possibly newly created).
         """
         if key is None:
-            # No key association, MsgTree is in MODE_DEFER
-            return self.children.setdefault(msgline, \
-                self.__class__(msgline, self, self._shift == self._shift_trace))
+            # no key association: MsgTree is in MODE_DEFER
+            if msgline in self.children:
+                return self.children[msgline]
+            else:
+                new_elem = self.__class__(msgline, self,
+                                          self._shift == self._shift_trace)
+                self.children[msgline] = new_elem
+                return new_elem
         else:
             # key given: get/create new child element and shift down the key
-            return self._shift(key, self.children.setdefault(msgline, \
-                self.__class__(msgline, self,
-                               self._shift == self._shift_trace)))
+            if msgline in self.children:
+                return self._shift(key, self.children[msgline])
+            else:
+                new_elem = self.__class__(msgline, self,
+                                          self._shift == self._shift_trace)
+                self.children[msgline] = new_elem
+                return self._shift(key, new_elem)
 
 
 class MsgTree(object):
@@ -177,7 +180,7 @@ class MsgTree(object):
 
     def __init__(self, mode=MODE_DEFER):
         """MsgTree initializer
-        
+
         The `mode' parameter should be set to one of the following constant:
 
         MODE_DEFER: all messages are processed immediately, saving memory from
@@ -249,11 +252,11 @@ class MsgTree(object):
         return self._keys.iterkeys()
 
     __iter__ = keys
-    
+
     def messages(self, match=None):
         """Return an iterator over MsgTree's messages."""
         return imap(itemgetter(0), self.walk(match))
-    
+
     def items(self, match=None, mapper=None):
         """
         Return (key, message) for each key of the MsgTree.
@@ -271,14 +274,14 @@ class MsgTree(object):
         """
         depth = 0
         # stack of (element, depth) tuples used to walk the tree
-        estack = [ (self._root, depth) ]
+        estack = [(self._root, depth)]
 
         while estack:
             elem, edepth = estack.pop()
             if len(elem.children) > 0:
                 estack += [(v, edepth + 1) for v in elem.children.values()]
             depth = max(depth, edepth)
-        
+
         return depth
 
     def walk(self, match=None, mapper=None):
@@ -291,7 +294,7 @@ class MsgTree(object):
         if self.mode == MODE_DEFER:
             self._update_keys()
         # stack of elements used to walk the tree (depth-first)
-        estack = [ self._root ]
+        estack = [self._root]
         while estack:
             elem = estack.pop()
             children = elem.children
@@ -313,7 +316,7 @@ class MsgTree(object):
         assert self.mode == MODE_TRACE, \
             "walk_trace() is only callable in trace mode"
         # stack of (element, depth) tuples used to walk the tree
-        estack = [ (self._root, 0) ]
+        estack = [(self._root, 0)]
         while estack:
             elem, edepth = estack.pop()
             children = elem.children
@@ -335,7 +338,7 @@ class MsgTree(object):
         """
         # do not walk tree in MODE_DEFER as no key is associated
         if self.mode != MODE_DEFER:
-            estack = [ self._root ]
+            estack = [self._root]
             # walk the tree to keep only matching keys
             while estack:
                 elem = estack.pop()

tests/MsgTreeTest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # ClusterShell test suite
-# Written by S. Thiell 2010-02-03
+# Written by S. Thiell
 
 
 """Unit test for ClusterShell MsgTree Class"""
@@ -182,12 +182,36 @@ class MsgTreeTest(unittest.TestCase):
         """test MsgTree scalability"""
         # build tree...
         tree = MsgTree()
-        for i in xrange(0, 10000):
+        for i in range(10000):
             tree.add("node%d" % i, "message%d" % i)
         self.assertEqual(len(tree), 10000)
         cnt = 0
         for msg, keys in tree.walk():
             cnt += 1
+        self.assertEqual(cnt, 10000)
+
+        tree = MsgTree()
+        for i in range(10000):
+            tree.add("nodeX", "message%d" % i)
+        self.assertEqual(len(tree), 1)
+        cnt = 0
+        for msg, keys in tree.walk():
+            testlines = str(msg) # calls MsgTreeElem.__iter__()
+            self.assertEqual(len(testlines.splitlines()), 10000)
+            cnt += 1
+        self.assertEqual(cnt, 1)
+
+        tree = MsgTree()
+        for j in range(100):
+            for i in range(1000):
+                tree.add("node%d" % j, "message%d" % i)
+        self.assertEqual(len(tree), 100)
+        cnt = 0
+        for msg, keys in tree.walk():
+            testlines = str(msg) # calls MsgTreeElem.__iter__()
+            self.assertEqual(len(testlines.splitlines()), 1000)
+            cnt += 1
+        self.assertEqual(cnt, 1)
 
     def test_007_shift_mode(self):
         """test MsgTree in shift mode"""

ebabbe/op-clustershell

    Significant performance improvement when lines added to the tree from
    was mistakenly used here, as its second argument is always evaluated
    even if the key already exists. This led to the creation of useless
    Benchmarking shows a speed of 50 to 75% for MsgTree.add() when used
    with multiple nodes.

lib/ClusterShell/MsgTree.py
@@ -1,6 +1,6 @@
 #
-# Copyright CEA/DAM/DIF (2007-2014)
-#  Contributor: Stephane THIELL <stephane.thiell@cea.fr>
+# Copyright CEA/DAM/DIF (2007-2016)
+#  Contributor: Stephane THIELL <sthiell@stanford.edu>
 #
 # This file is part of the ClusterShell library.
 #
@@ -70,7 +70,7 @@ class MsgTreeElem(object):
         # content
         self.msgline = msgline
         self.keys = None
-   
+
     def __len__(self):
         """Length of whole message string."""
         return len(str(self))
@@ -92,7 +92,7 @@ class MsgTreeElem(object):
             shifting = self.keys
             self.keys = None
         else:
-            shifting = set([ key ])
+            shifting = set([key])
             if self.keys:
                 self.keys.difference_update(shifting)
 
@@ -107,7 +107,7 @@ class MsgTreeElem(object):
         """Shift one of our key to specified target element (trace
         mode: keep backtrace of keys)."""
         if not target_elem.keys:
-            target_elem.keys = set([ key ])
+            target_elem.keys = set([key])
         else:
             target_elem.keys.add(key)
         return target_elem
@@ -116,24 +116,18 @@ class MsgTreeElem(object):
         return list(self.lines())[i]
 
     def __iter__(self):
-        """Iterate over message lines starting from this tree element."""
-        # no msgline in root element
-        if self.msgline is None:
-            return
-        # trace the message path
-        path = [self.msgline]
-        parent = self.parent
-        while parent.msgline is not None:
-            path.append(parent.msgline)
-            parent = parent.parent
-        # rewind path
-        while path:
-            yield path.pop()
+        """Iterate over message lines up to this element."""
+        bottomtop = []
+        if self.msgline is not None:
+            bottomtop.append(self.msgline)
+            parent = self.parent
+            while parent.msgline is not None:
+                bottomtop.append(parent.msgline)
+                parent = parent.parent
+        return reversed(bottomtop)
 
     def lines(self):
-        """
-        Get the whole message lines iterator from this tree element.
-        """
+        """Get an iterator over all message lines up to this element."""
         return iter(self)
 
     splitlines = lines
@@ -153,15 +147,20 @@ class MsgTreeElem(object):
         optional associated source key. Called by MsgTree.add().
         Return corresponding MsgTreeElem (possibly newly created).
         """
+        # get/create child element
+        elem = self.children.get(msgline)
+        if elem is None:
+            elem = self.__class__(msgline, self,
+                                  self._shift == self._shift_trace)
+            self.children[msgline] = elem
+
+        # if no key is given, MsgTree is in MODE_DEFER
+        # shift down the given key otherwise
+        # Note: replace with ternary operator in py2.5+
         if key is None:
-            # No key association, MsgTree is in MODE_DEFER
-            return self.children.setdefault(msgline, \
-                self.__class__(msgline, self, self._shift == self._shift_trace))
+            return elem
         else:
-            # key given: get/create new child element and shift down the key
-            return self._shift(key, self.children.setdefault(msgline, \
-                self.__class__(msgline, self,
-                               self._shift == self._shift_trace)))
+            return self._shift(key, elem)
 
 
 class MsgTree(object):
@@ -177,7 +176,7 @@ class MsgTree(object):
 
     def __init__(self, mode=MODE_DEFER):
         """MsgTree initializer
-        
+
         The `mode' parameter should be set to one of the following constant:
 
         MODE_DEFER: all messages are processed immediately, saving memory from
@@ -249,11 +248,11 @@ class MsgTree(object):
         return self._keys.iterkeys()
 
     __iter__ = keys
-    
+
     def messages(self, match=None):
         """Return an iterator over MsgTree's messages."""
         return imap(itemgetter(0), self.walk(match))
-    
+
     def items(self, match=None, mapper=None):
         """
         Return (key, message) for each key of the MsgTree.
@@ -271,14 +270,14 @@ class MsgTree(object):
         """
         depth = 0
         # stack of (element, depth) tuples used to walk the tree
-        estack = [ (self._root, depth) ]
+        estack = [(self._root, depth)]
 
         while estack:
             elem, edepth = estack.pop()
             if len(elem.children) > 0:
                 estack += [(v, edepth + 1) for v in elem.children.values()]
             depth = max(depth, edepth)
-        
+
         return depth
 
     def walk(self, match=None, mapper=None):
@@ -291,7 +290,7 @@ class MsgTree(object):
         if self.mode == MODE_DEFER:
             self._update_keys()
         # stack of elements used to walk the tree (depth-first)
-        estack = [ self._root ]
+        estack = [self._root]
         while estack:
             elem = estack.pop()
             children = elem.children
@@ -313,7 +312,7 @@ class MsgTree(object):
         assert self.mode == MODE_TRACE, \
             "walk_trace() is only callable in trace mode"
         # stack of (element, depth) tuples used to walk the tree
-        estack = [ (self._root, 0) ]
+        estack = [(self._root, 0)]
         while estack:
             elem, edepth = estack.pop()
             children = elem.children
@@ -335,7 +334,7 @@ class MsgTree(object):
         """
         # do not walk tree in MODE_DEFER as no key is associated
         if self.mode != MODE_DEFER:
-            estack = [ self._root ]
+            estack = [self._root]
             # walk the tree to keep only matching keys
             while estack:
                 elem = estack.pop()

tests/MsgTreeTest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # ClusterShell test suite
-# Written by S. Thiell 2010-02-03
+# Written by S. Thiell
 
 
 """Unit test for ClusterShell MsgTree Class"""
@@ -180,14 +180,40 @@ class MsgTreeTest(unittest.TestCase):
 
     def test_006_scalability(self):
         """test MsgTree scalability"""
-        # build tree...
+        # test tree of 10k nodes with a single different line each
         tree = MsgTree()
-        for i in xrange(0, 10000):
+        for i in range(10000):
             tree.add("node%d" % i, "message%d" % i)
         self.assertEqual(len(tree), 10000)
         cnt = 0
         for msg, keys in tree.walk():
             cnt += 1
+        self.assertEqual(cnt, 10000)
+
+        # test tree of 1 node with 10k lines
+        tree = MsgTree()
+        for i in range(10000):
+            tree.add("nodeX", "message%d" % i)
+        self.assertEqual(len(tree), 1)
+        cnt = 0
+        for msg, keys in tree.walk():
+            testlines = str(msg) # test MsgTreeElem.__iter__()
+            self.assertEqual(len(testlines.splitlines()), 10000)
+            cnt += 1
+        self.assertEqual(cnt, 1)
+
+        # test tree of 100 nodes with the same 1000 message lines each
+        tree = MsgTree()
+        for j in range(100):
+            for i in range(1000):
+                tree.add("node%d" % j, "message%d" % i)
+        self.assertEqual(len(tree), 100)
+        cnt = 0
+        for msg, keys in tree.walk():
+            testlines = str(msg) # test MsgTreeElem.__iter__()
+            self.assertEqual(len(testlines.splitlines()), 1000)
+            cnt += 1
+        self.assertEqual(cnt, 1)
 
     def test_007_shift_mode(self):
         """test MsgTree in shift mode"""